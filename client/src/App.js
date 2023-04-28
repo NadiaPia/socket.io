8ca738f59f19ba0eee13ffd9d72bcc129d1d0538
NadiaPia <206i417@gmail.com>
2023-04-28 10:45:08 -0700

    Add functinality to emit and to receive messages btw FE and BE

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import io from "socket.io-client";
+import {useEffect, useState} from "react";
+
+const socket = io.connect("http://localhost:3001"); //connection with the BE
 
 function App() {
 
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message });  //socket.emit() - we send a message to the BE 
+  };
+
+  useEffect(() => { //will run every time any event is thrown to us on a socket.io server (when event is emited this function will run)
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+ 
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+          <input onChange={(event) => {setMessage(event.target.value);}} placeholder="Message..."/>
+          <button onClick={sendMessage}>Send Message</button>
+          <h1>Message:{messageReceived}</h1>
+            
         </div>
     );
 }
--- server/index.js
+++ server/index.js
@@ -1,3 +1,38 @@
-const a = 4;
-a;
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",  //connection with the FE
+        methods: ["GET", "POST"],
+    },
+});
+
+//Starting listening some events coming from the FE:
+
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`); //User connected: O47tlsRvDakptmhtAAAJ - every time after user come in website
+
+    socket.on("send_message", (data) => {  //it is listening the event called "send_message" and this event will contain some data
+        //console.log(data); //we want to emit this data to everyone, who is connected to this server, but not yourself
+        socket.broadcast.emit("receive_message", data); //broadcast allows us to sent data anyone but 
+        //yourself. We will listening to this in the FE so that we can receive all the messages that were emited by other people.
+    });
+
+}); //.on means listening for the event, "connection" - the type of the event. There is a 
+//user, who is connecting to the server, like someone opens the webside and going to the 
+//client, then this code will emmediatelly start running.
+
+//socket variable that we can get from there, give us a lot of
+// info: f.e we can console.log when the user is connecting
+
+
+server.listen(3001, () => {
+    console.log("SERVER IS RUNNING ON PORT 3001");
+});
